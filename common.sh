18f81891550b4013c9b52f32c56a947f69620bd2
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 04:32:05 +0530

    frontend setup using shell script

--- common.sh
+++ common.sh
@@ -1,6 +1,18 @@
+status(){
+if [ $? -eq 0 ]; then
+  echo "${GC}Success${NC}"
+  else
+  echo "${RC}Failure${NC}"
+fi
+
+}
+
+
+
 RC="\e[31m"
 YC="\e[33m"
 GC="\e[32m"
 NC="\e[0m"
 
 OUTPUT=/tmp/wmp.log
+
--- frontend.sh
+++ frontend.sh
@@ -2,45 +2,60 @@ source common.sh
 
 echo "${YC}Disable Nginx${NC}"
 dnf module disable nginx -y  &>>OUTPUT
+status $?
 
 echo "${YC}Enable Nginx${NC}"
-dnf module enable nginx:1.26 -y
+dnf module enable nginx:1.26 -y &>>OUTPUT
+status $?
 
 echo "${YC}Install Nginx${NC}"
-dnf install -y nginx
+dnf install -y nginx &>>OUTPUT
+status $?
 
 echo "${YC}Copy nginx config file${NC}"
-cp nginx.conf /etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf &>>OUTPUT
+status $?
 
 echo "${YC}Download and nodejs 22 repo${NC}"
-curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
-dnf install -y nodejs
+curl -fsSL https://rpm.nodesource.com/setup_22.x | bash - &>>OUTPUT
+status $?
+
+echo "${YC}Install nodejs${NC}"
+dnf install -y nodejs &>>OUTPUT
+status $?
 
 echo "${YC}Download App content${NC}"
-curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz
+curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz &>>OUTPUT
+status $?
 
 echo "${YC}Create Directory${NC}"
 mkdir -p /tmp/frontend
+status $?
 
 echo "${YC}Extract App content${NC}"
 cd /tmp/frontend
-tar xzf /tmp/frontend.tar.gz
+tar xzf /tmp/frontend.tar.gz &>>OUTPUT
+status $?
 
 echo "${YC}Download Dependencies${NC}"
 cd /tmp/frontend
-npm ci
-npm run build
+npm ci &>>OUTPUT
+npm run build &>>OUTPUT
+status $?
 
 echo "${YC}Remove default content${NC}"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>OUTPUT
+status $?
 
 echo "${YC}Copy App contents ${NC}"
 cp -r /tmp/frontend/dist/* /usr/share/nginx/html/
+status $?
 
 echo "${YC}Enable Nginx${NC}"
-systemctl enable nginx
+systemctl enable nginx &>>OUTPUT
+status $?
 
 echo "${YC}Start Nginx${NC}"
 systemctl start nginx
-
+status $?
 
