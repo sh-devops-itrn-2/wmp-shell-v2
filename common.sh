a069f3e431e98b1e273727ab57fa8fafc79c57c4
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 12:40:43 +0530

    functions added in common.sh and called in portfolio service

--- common.sh
+++ common.sh
@@ -17,13 +17,30 @@ fi
 
 app_pre_req(){
   mkdir -p /app
-  curl -L -o /tmp/${service_name}.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/${service_name}.tar.gz
+  curl -L -o /tmp/${service_name}.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/${service_name}.tar.gz &>>OUTPUT
   cd /app
-  tar xzf /tmp/${service_name}.tar.gz
+  tar xzf /tmp/${service_name}.tar.gz &>>OUTPUT
 
 }
+set_ownership(){
+  echo -e "${YC}Set ownership and permission${NC}"
+  chown -R appuser:appuser /app
+  chmod o-rwx /app -R
 
+}
+copy_systemd_file(){
+  echo -e "${YC}Copy systemd file${NC}"
+  cp -r portfolio-service.service /etc/systemd/system/portfolio-service.service
+  status
+}
+service_start(){
+  echo -e "${YC}${service_name} Started${NC}"
+  systemctl daemon-reload
+  systemctl enable ${service_name} &>>OUTPUT
+  systemctl start ${service_name}
+  status
 
+}
 RC="\e[31m"
 YC="\e[33m"
 GC="\e[32m"
--- portfolio-service.sh
+++ portfolio-service.sh
@@ -1,31 +1,15 @@
 source common.sh
 service_name=portfolio-service
+
 echo -e "${YC}Install Java 21${NC}"
 dnf install -y java-21-openjdk-devel &>>OUTPUT
 status
 
 user_add
 
-#echo -e "${YC}Create Directory${NC}" &>>OUTPUT
-#mkdir -p /app
-#status
+copy_systemd_file
 
-#echo -e "${YC}Download App Content${NC}"
-#curl -L -o /tmp/portfolio-service.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/portfolio-service.tar.gz &>>OUTPUT
-#status
 app_pre_req
-
-echo -e "${YC}Copy systemd file${NC}"
-cp -r portfolio-service.service /etc/systemd/system/portfolio-service.service
-status
-
-#echo -e "${YC}Change Directory${NC}"
-#cd /app
-#status
-#
-#echo -e "${YC}Extract App Content${NC}"
-#tar xzf /tmp/portfolio-service.tar.gz &>>OUTPUT
-#status
 
 echo -e "${YC}Build Application${NC}"
 ./gradlew bootJar --no-daemon -x test &>>OUTPUT
@@ -35,18 +19,6 @@ echo -e "${YC}Copy the built JAR${NC}"
 cp /app/build/libs/*.jar /app/portfolio-service.jar
 status
 
-echo -e "${YC}Set ownership${NC}"
-chown -R appuser:appuser /app
-status
+set_ownership
 
-echo -e "${YC}Remove Others Permission${NC}"
-chmod o-rwx /app -R
-status
-
-
-echo -e "${YC}Portfolio Service Started"
-systemctl daemon-reload
-systemctl enable portfolio-service &>>OUTPUT
-systemctl start portfolio-service
-status
-
+service_start
