edd6b7aac864a5bb52d3ed44aedfd209d7a96a5a
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 14:01:37 +0530

    frontend code replaced with 88 code

--- frontend.sh
+++ frontend.sh
@@ -1,61 +1,43 @@
 source common.sh
 
-echo -e "${YC}Disable Nginx${NC}"
-dnf module disable nginx -y  &>>OUTPUT
-status $?
-
-echo -e "${YC}Enable Nginx${NC}"
-dnf module enable nginx:1.26 -y &>>OUTPUT
-status $?
-
 echo -e "${YC}Install Nginx${NC}"
-dnf install -y nginx &>>OUTPUT
-status $?
+dnf module disable nginx -y &>>$OUTPUT
+dnf module enable nginx:1.26 -y &>>$OUTPUT
+dnf install -y nginx &>>$OUTPUT
+status_check
 
-echo -e "${YC}Copy nginx config file${NC}"
-cp nginx.conf /etc/nginx/nginx.conf &>>OUTPUT
-status $?
+echo -e "${YC}Update Nginx Config${NC}"
+cp nginx.conf /etc/nginx/nginx.conf &>>$OUTPUT
+status_check
 
-echo -e "${YC}Download and nodejs 22 repo${NC}"
-curl -fsSL https://rpm.nodesource.com/setup_22.x | bash - &>>OUTPUT
-status $?
+echo -e "${YC}Install NodeJS${NC}"
+curl -fsSL https://rpm.nodesource.com/setup_22.x | bash - &>>$OUTPUT
+dnf install -y nodejs &>>$OUTPUT
 
-echo -e "${YC}Install nodejs${NC}"
-dnf install -y nodejs &>>OUTPUT
-status $?
+node --version &>>$OUTPUT
+npm --version &>>$OUTPUT
+status_check
 
-echo -e "${YC}Download App content${NC}"
-curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz &>>OUTPUT
-status $?
-
-echo -e "${YC}Create Directory${NC}"
-mkdir -p /tmp/frontend
-status $?
-
-echo -e "${YC}Extract App content${NC}"
+echo -e "${YC}Download and extract Application${NC}"
+curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz &>>$OUTPUT
+mkdir -p /tmp/frontend &>>$OUTPUT
 cd /tmp/frontend
-tar xzf /tmp/frontend.tar.gz &>>OUTPUT
-status $?
+tar xzf /tmp/frontend.tar.gz &>>$OUTPUT
+status_check
 
-echo -e "${YC}Download Dependencies${NC}"
+echo -e "${YC}Build frontend code${NC}"
 cd /tmp/frontend
-npm ci &>>OUTPUT
-npm run build &>>OUTPUT
-status $?
+npm ci &>>$OUTPUT
+npm run build &>>$OUTPUT
+status_check
 
-echo -e "${YC}Remove default content${NC}"
-rm -rf /usr/share/nginx/html/* &>>OUTPUT
-status $?
+echo -e "${YC}Update frontend code${NC}"
+rm -rf /usr/share/nginx/html/* &>>$OUTPUT
+cp -r /tmp/frontend/dist/* /usr/share/nginx/html/ &>>$OUTPUT
+status_check
 
-echo -e "${YC}Copy App contents ${NC}"
-cp -r /tmp/frontend/dist/* /usr/share/nginx/html/
-status $?
-
-echo -e "${YC}Enable Nginx${NC}"
-systemctl enable nginx &>>OUTPUT
-status $?
-
-echo -e "${YC}Start Nginx${NC}"
-systemctl start nginx
-status $?
+echo -e "${YC}Start nginx service${NC}"
+systemctl enable nginx &>>$OUTPUT
+systemctl restart nginx &>>$OUTPUT
+status_check
 
