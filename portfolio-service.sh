a46ee89a6a59a3f8984a1089ec1999563117f540
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 11:31:31 +0530

    portfolio service copied systemd and started service

--- portfolio-service.sh
+++ portfolio-service.sh
@@ -16,6 +16,10 @@ echo -e "${YC}Download App Content${NC}"
 curl -L -o /tmp/portfolio-service.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/portfolio-service.tar.gz &>>OUTPUT
 status
 
+echo -e "${YC}Copy systemd file{NC}"
+cp -r portfolio-service.service /etc/systemd/system/portfolio-service.service
+status
+
 echo -e "${YC}Change Directory${NC}"
 cd /app
 status
@@ -25,7 +29,7 @@ tar xzf /tmp/portfolio-service.tar.gz &>>OUTPUT
 status
 
 echo -e "${YC}Build Application${NC}"
-./gradlew bootJar --no-daemon -x test
+./gradlew bootJar --no-daemon -x test &>>OUTPUT
 status
 
 echo -e "${YC}Copy the built JAR${NC}"
@@ -41,5 +45,9 @@ chmod o-rwx /app -R
 status
 
 
-
+echo -e "${YC}Portfolio Service Started"
+systemctl daemon-reload
+systemctl enable portfolio-service &>>OUTPUT
+systemctl start portfolio-service
+status
 
