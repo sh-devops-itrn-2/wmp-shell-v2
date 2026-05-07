cb50d3d832a55fc78944fb9c3ef37570b5d6d47c
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 05:59:12 +0530

    redirected to OUTPUT

--- postgresql.sh
+++ postgresql.sh
@@ -5,14 +5,15 @@ dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_6
 status $?
 
 echo -e "${YC}Disable PostgreSQL${NC}"
-dnf -qy module disable postgresql
+dnf -qy module disable postgresql &>>OUTPUT
 status $?
 
 echo -e "${YC}Install PostgreSQL 16${NC}"
-dnf install -y postgresql16-server postgresql16
+dnf install -y postgresql16-server postgresql16 &>>OUTPUT
 status $?
 
 echo -e "${YC}Initialize Database${NC}"
-/usr/pgsql-16/bin/postgresql-16-setup initdb
+/usr/pgsql-16/bin/postgresql-16-setup initdb &>>OUTPUT
 status $?
+
 
