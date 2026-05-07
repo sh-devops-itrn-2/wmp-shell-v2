28c70ba9714dd1b644b3de8f77f94baa8a94d731
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 11:21:13 +0530

    portfolio service setup using shell-script

--- portfolio-service.sh
+++ portfolio-service.sh
@@ -3,3 +3,43 @@ source common.sh
 echo -e "${YC}Install Java 21${NC}"
 dnf install -y java-21-openjdk-devel &>>OUTPUT
 status
+
+echo -e "${YC}Add Application User${NC}"
+useradd -r -s /bin/false appuser &>>OUTPUT
+status
+
+echo -e "${YC}Create Directory${NC}" &>>OUTPUT
+mkdir -p /app
+status
+
+echo -e "${YC}Download App Content${NC}"
+curl -L -o /tmp/portfolio-service.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/portfolio-service.tar.gz &>>OUTPUT
+status
+
+echo -e "${YC}Change Directory${NC}"
+cd /app
+status
+
+echo -e "${YC}Extract App Content${NC}"
+tar xzf /tmp/portfolio-service.tar.gz &>>OUTPUT
+status
+
+echo -e "${YC}Build Application${NC}"
+./gradlew bootJar --no-daemon -x test
+status
+
+echo -e "${YC}Copy the built JAR${NC}"
+cp /app/build/libs/*.jar /app/portfolio-service.jar
+status
+
+echo -e "${YC}Set ownership${NC}"
+chown -R appuser:appuser /app
+status
+
+echo -e "${YC}Remove Others Permission${NC}"
+chmod o-rwx /app -R
+status
+
+
+
+
