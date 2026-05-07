5beb4b20571454d4674df395099c46e45c8a44e4
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 05:56:57 +0530

    initialize db

--- postgresql.sh
+++ postgresql.sh
@@ -4,3 +4,15 @@ echo -e "${YC}Install PostgreSQL Repo"
 dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm &>>OUTPUT
 status $?
 
+echo -e "${YC}Disable PostgreSQL${NC}"
+dnf -qy module disable postgresql
+status $?
+
+echo -e "${YC}Install PostgreSQL 16${NC}"
+dnf install -y postgresql16-server postgresql16
+status $?
+
+echo -e "${YC}Initialize Database${NC}"
+/usr/pgsql-16/bin/postgresql-16-setup initdb
+status $?
+
