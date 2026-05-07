00ed6da090c4fbd0a3982f65f45488e0ab9cdf44
shujath-git900 <shujath.devops@gmail.com>
2026-05-07 12:46:29 +0530

    status function called

--- common.sh
+++ common.sh
@@ -7,6 +7,7 @@ fi
 
 }
 user_add(){
+echo "${YC}Adding User${NC}"
 id appuser &>/dev/null
 if [ $? -ne 0 ]; then
 useradd -r -s /bin/false appuser &>>OUTPUT
@@ -26,7 +27,7 @@ set_ownership(){
   echo -e "${YC}Set ownership and permission${NC}"
   chown -R appuser:appuser /app
   chmod o-rwx /app -R
-
+  status
 }
 copy_systemd_file(){
   echo -e "${YC}Copy systemd file${NC}"
